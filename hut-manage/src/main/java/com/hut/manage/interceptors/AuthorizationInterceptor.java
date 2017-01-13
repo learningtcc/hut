66ad4da61b68abe3889abc09158bab108d9292ae
Jared <15011290963@163.com>
2017-01-13 17:51:23 +0800

    13

---
+++ hut-manage/src/main/java/com/hut/manage/interceptors/AuthorizationInterceptor.java
@@ -0,0 +1,15 @@
+package com.hut.manage.interceptors;
+
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
+
+/**
+ * Created by Jared on 2017/1/13.
+ */
+public class AuthorizationInterceptor extends AbstractInterceptor {
+
+    @Override
+    public String intercept(ActionInvocation actionInvocation) throws Exception {
+        return null;
+    }
+}
